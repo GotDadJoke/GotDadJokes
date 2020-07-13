000e4e61b493155323799fd5aafddc2ec0fcb21c
charlesmsy <charles.m.sy@hotmail.com>
2020-07-12 18:26:56 -0700

    Updated HTML, CSS, JS

---
+++ GotDadJokes-app/public/js/index.js
@@ -0,0 +1,14 @@
+const api_url = 'https://sv443.net/jokeapi/v2/joke/Any'
+
+
+async function findDadJokes(){
+
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+    if(!data.joke)
+        document.getElementById("jokeLabelBtn").innerHTML = data.setup +" "+ data.delivery;
+    else
+        document.getElementById("jokeLabelBtn").innerHTML = data.joke;
+    
+}
