d32141196326818fee0eb47b342aea24855c5afb
charlesmsy <charles.m.sy@hotmail.com>
2020-07-13 14:20:10 -0700

    Added responsive window.

--- GotDadJokes-app/public/js/index.js
+++ GotDadJokes-app/public/js/index.js
@@ -1,5 +1,5 @@
 //Currently not using the correct API. 
-//https://icanhazdadjoke.com/
+//const api_url = 'https://icanhazdadjoke.com/'
 const api_url = 'https://sv443.net/jokeapi/v2/joke/Any'
 
 
@@ -7,7 +7,8 @@ async function findDadJokes(){
 
     const response = await fetch(api_url);
     const data = await response.json();
-    console.log(data);
+	console.log(data);
+	
     if(!data.joke)
         document.getElementById("jokeLabelBtn").innerHTML = data.setup +" "+ data.delivery;
     else
